9eab8ffcffd9a88ce9874deec5d2780d0e8a066d
khan-shuvo <shuvo2004@outlook.com>
2026-02-03 17:54:25 +0600

    add all section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { About } from './components/About'
+import { Contact } from './components/Contact'
+import { Footer } from './components/Footer'
+import { Hero } from './components/Hero'
 import { Navigation } from './components/Navigation'
+import { Skills } from './components/Skills'
 
 function App() {
   return (
     <>
       <div className='min-h-screen bg-white dark:bg-gray-900 transition-colors'>
         <Navigation />
+        <main>
+          <Hero />
+          <About />
+          <Skills />
+          <Contact />
+          <Footer />
+        </main>
       </div>
     </>
   )
