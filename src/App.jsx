711571217a717b93cf89d6e68fa11245da6c445e
khan-shuvo <shuvo2004@outlook.com>
2026-02-03 19:46:36 +0600

    add experience section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { About } from './components/About'
 import { Contact } from './components/Contact'
+import { Experience } from './components/Experience'
 import { Footer } from './components/Footer'
 import { Hero } from './components/Hero'
 import { Navigation } from './components/Navigation'
@@ -14,6 +15,7 @@ function App() {
           <Hero />
           <About />
           <Skills />
+          <Experience />
           <Contact />
           <Footer />
         </main>
